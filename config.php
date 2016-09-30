c498d0d4bba092d83af4a978dbecc0166b93cf9d
alex <answer3ster@gmail.com>
2016-09-30 20:37:50 +0200

    config

--- config.php
+++ config.php
@@ -2,13 +2,13 @@
 
 define('DB_ENGINE', 'mysql');
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
 
-define('DB_NAME', 'pt_test');
+define('DB_NAME', 'heroku_af7477a27472822');
 
-define('DB_USER','root');
+define('DB_USER','bcc4a9d7499576');
 
-define('DB_PASS','');
+define('DB_PASS','f07ae4ac');
 
 define('API_TOKEN','e7b6b0c52ac44f0236a3125c18b4454c77f78cd2');
 
